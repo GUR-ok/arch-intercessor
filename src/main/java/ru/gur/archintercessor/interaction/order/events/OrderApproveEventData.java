1fdee5614c9d500ba3ae8588bb4955571e18d9ee
Yuriy Gorelov <gur-ok@yandex.ru>
2022-11-04 17:14:05 +0400

    fix json serialization

--- src/main/java/ru/gur/archintercessor/interaction/order/events/OrderApproveEventData.java
+++ src/main/java/ru/gur/archintercessor/interaction/order/events/OrderApproveEventData.java
@@ -1,17 +1,19 @@
 package ru.gur.archintercessor.interaction.order.events;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
-import lombok.Value;
+import lombok.Data;
 
 import java.util.UUID;
 
-@Value
+@Data
 @Builder
 public class OrderApproveEventData implements KafkaEvent {
 
     UUID orderId;
 
     @Override
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) // Prevents duplication when serializing to JSON (subtype discriminator property)
     public Event getEvent() {
         return Event.ORDER_APPROVE;
     }
--- src/main/java/ru/gur/archintercessor/interaction/order/events/OrderCancelEventData.java
+++ src/main/java/ru/gur/archintercessor/interaction/order/events/OrderCancelEventData.java
@@ -1,19 +1,21 @@
 package ru.gur.archintercessor.interaction.order.events;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Value;
 
 import java.util.UUID;
 
-@Value
+@Data
 @Builder
 public class OrderCancelEventData implements KafkaEvent {
 
     UUID orderId;
 
     @Override
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) // Prevents duplication when serializing to JSON (subtype discriminator property)
     public Event getEvent() {
         return Event.ORDER_CANCEL;
     }
 }
-
